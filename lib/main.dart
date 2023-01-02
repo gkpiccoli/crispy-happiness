99e1efd23544a6a18c6a41addab6289729288c4f
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-02 16:28:01 -0300

    first screen

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Home Page',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
+        ),
+      ),
+    );
+  }
+}
