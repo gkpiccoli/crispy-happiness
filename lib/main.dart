204de45c04c70c833fe09b103c46fc195f5e66d1
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-03 10:30:38 -0300

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,19 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Home Page',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
-          title: const Text('Flutter: Desenhando na tela'),
-        ),
-        body: GridView.count(
-          primary: false,
-          crossAxisCount: 5,
-          padding: EdgeInsets.all(8.0),
-          children: [
-            Container(
-              color: Colors.teal,
-            )
-          ],
-        ),
-      ),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            leading: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
+            title: const Text('Flutter: Desenhando na tela'),
+          ),
+          body: ListView(
+            children: [
+              Container(),
+              Container(),
+              Container(),
+            ],
+          )),
     );
   }
 }
