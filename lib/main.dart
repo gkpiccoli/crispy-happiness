482d5216a8bf1d1e1936395d660a4ffacfea0f2a
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-02 22:24:33 -0300

    first container :D

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,16 @@ class MyApp extends StatelessWidget {
               onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
           title: const Text('Flutter: Desenhando na tela'),
         ),
+        body: GridView.count(
+          primary: false,
+          crossAxisCount: 5,
+          padding: EdgeInsets.all(8.0),
+          children: [
+            Container(
+              color: Colors.teal,
+            )
+          ],
+        ),
       ),
     );
   }
