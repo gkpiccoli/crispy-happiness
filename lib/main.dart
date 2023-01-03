76a8861e5d68338958450863cd3bead013167c2a
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-03 13:03:55 -0300

    stl

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,43 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Home Page',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            leading: IconButton(
-                onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
-            title: const Text('Flutter: Desenhando na tela'),
-          ),
-          body: ListView(
-            children: [
-              Container(),
-              Container(),
-              Container(),
-            ],
-          )),
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {}, icon: const Icon(Icons.add_task_rounded)),
+          title: const Text('Flutter: Desenhando na tela'),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              color: Colors.blue,
+              child: const Text('Agua'),
+            ),
+            Container(
+              color: Colors.green,
+              child: const Text('Gelo'),
+            ),
+            Container(
+              color: Colors.red,
+              child: const Text('Vapor'),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class BlocosColoridos extends StatelessWidget {
+  const BlocosColoridos(
+      {super.key, required this.cor1, required this.cor2, required this.cor3});
+
+  final Color cor1;
+  final Color cor2;
+  final Color cor3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
